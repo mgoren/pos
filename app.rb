bc0b10c90cd432fd149b266b1a53c5739a2f22df
Mike Goren & Karvari Ellingson <student@epicodus.com>
2015-01-27 14:01:23 -0800

    add update feature to manager's product page

--- app.rb
+++ app.rb
@@ -4,18 +4,39 @@ require("pg")
 also_reload("lib/**/*.rb")
 require("sinatra/activerecord")
 require("./lib/product")
+require('pry')
 
 get("/") do
   erb(:index)
 end
 
 get("/manager/products") do
-    erb(:manager_products)
+  erb(:manager_products)
+end
+
+get("/manager/products/:id/edit") do
+  @product = Product.find(params.fetch("id").to_i())
+  erb(:product_edit)
+end
+
+patch("/manager/products/:id") do
+  product = Product.find(params.fetch("id").to_i())
+  new_name = params["new_name"]
+  new_price = params["new_price"].to_f()
+
+  if new_name == ""
+    new_name = product.name()
+  end
+  if new_price <= 0.0
+    new_price = product.price()
+  end  
+  product.update({ :name => new_name, :price => new_price })
+  redirect("/manager/products")
 end
 
 post("/manager/products") do
   product_name = params.fetch("product_name")
-  price = params.fetch("price")
+  price = params.fetch("price").to_f()
   @product = Product.new({ :name => product_name, :price => price })
   if @product.save()
     redirect("/manager/products")
--- lib/product.rb
+++ lib/product.rb
@@ -1,8 +1,7 @@
 class Product < ActiveRecord::Base
   has_and_belongs_to_many :purchases
-  validates_presence_of :name, :price
-  validates :price, numericality: {greater_than: 0.0}
-
+  validates :name, { :presence => true }
+  validates :price, { :presence => true, :numericality => { :greater_than => 0.0 } }
 
 
 
